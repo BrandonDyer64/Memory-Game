47f1b33b79836f98a30ccdb01a30b3a1e9be6131
Brandon Dyer <brandondyer64@gmail.com>
2019-03-21 14:41:19 -0500

    Added card flipping animations

---
+++ src/Game/Card.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div style={{ margin: '10px' }}>
+        <div
+          className='flip-container'
+          onClick={event => {
+            event.currentTarget.classList.toggle('hover')
+          }}
+        >
+          <div className='flipper'>
+            <div className='flip-front'>
+              <div className='card'>
+                <div className='card-back' />
+              </div>
+            </div>
+
+            <div className='flip-back'>
+              <div className='card'>
+                <div className='card-front'>
+                  <span className='suit' style={{ textAlign: 'left' }}>
+                    ♦
+                  </span>
+                  <span style={{ fontSize: '2em' }}>{this.props.number}</span>
+                  <span className='suit' style={{ textAlign: 'right' }}>
+                    ♦
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/Game/index.js
+++ src/Game/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Card from './Card'
+import { range, shuffleArray } from '../Utils'
 import './GameBoard.css'
 import './Cards.css'
 
@@ -6,8 +8,10 @@ export default class Game extends Component {
   constructor(props) {
     super(props)
     const cards = []
+    const cardNumbers = [...range(0, 12), ...range(0, 12)]
+    shuffleArray(cardNumbers)
     for (var i = 0; i < 24; i++) {
-      cards.push(<div class='card' />)
+      cards.push(<Card number={cardNumbers[i]} />)
     }
     this.state = {
       cards
