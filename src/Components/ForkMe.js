b2dabdf0698bbb3361e2822166186700e1c959ff
Brandon Dyer <brandondyer64@gmail.com>
2019-03-29 10:12:48 -0500

    Removed reference to interview homework

--- src/Components/ForkMe.js
+++ src/Components/ForkMe.js
@@ -4,7 +4,7 @@ import './ForkMe.css'
 export default function ForkMe(props) {
   return (
     <span id='forkongithub'>
-      <a href='https://github.com/BrandonDyer64/grhomework' target='_blank'>
+      <a href='https://github.com/BrandonDyer64/Memory-Game' target='_blank'>
         Fork me on GitHub
       </a>
     </span>
