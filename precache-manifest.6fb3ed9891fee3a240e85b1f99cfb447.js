1c19909dbbae6dc4004dd473d9ad5d4083fcb2de
Brandon Dyer <BrandonDyer64@gmail.com>
2019-12-31 01:31:06 -0600

    Updates

---
+++ precache-manifest.6fb3ed9891fee3a240e85b1f99cfb447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1972c88170b918b56e0d46c0d22762f0",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "1f636bf67a243504fcc3",
+    "url": "/Memory-Game/static/css/main.e3f6cca4.chunk.css"
+  },
+  {
+    "revision": "efd3a9f24b8aa181042e",
+    "url": "/Memory-Game/static/js/2.999db7a1.chunk.js"
+  },
+  {
+    "revision": "1f636bf67a243504fcc3",
+    "url": "/Memory-Game/static/js/main.c9a05689.chunk.js"
+  },
+  {
+    "revision": "7126f08e50ba83a4f18e",
+    "url": "/Memory-Game/static/js/runtime-main.525ab817.js"
+  },
+  {
+    "revision": "78117eb5f6d9a965ff359f7321686341",
+    "url": "/Memory-Game/static/media/cardback.78117eb5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Memory-Game/precache-manifest.91a0adb8780d8588961ca6ab90bb5557.js"
+  "/Memory-Game/precache-manifest.6fb3ed9891fee3a240e85b1f99cfb447.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Memory-Game/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Memory-Game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
